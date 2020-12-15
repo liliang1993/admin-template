e75d2ae8ddeed94844e7a6857dae61f64ced2681
李亮 <7963564+liliang199310@user.noreply.gitee.com>
2020-12-15 19:57:40 +0800

    feat(*): 搭建ant-design-vue-pro框架

---
+++ src/components/TinyMceEditor/index.vue
@@ -0,0 +1,50 @@
+<template>
+    <div>
+        
+    </div>
+</template>
+
+<script>
+    import Editor from "@tinymce/tinymce-vue"
+    //
+    export default {
+        name: "TinyMceEditor",
+        components: {Editor},
+        data() {
+            return {
+                //编辑器配置
+                editorInit：{
+                    language_url: "/zh_CN.js",
+                    language: "zh_CN",
+                    height: 500,
+                    menubar: true,
+                    plugins: [
+                        "advlist autolink lists link image charmap print preview anchor",
+                        "searchreplace visualblocks code fullscreen",
+                        "insertdatetime media table paste code help wordcount",
+                        "image",
+                    ],
+                    toolbar:
+                        "undo redo | formatselect | bold italic backcolor | \
+                        alignleft aligncenter alignright alignjustify | \
+                        bullist numlist outdent indent | removeformat | image table",
+                    branding: false,
+                     // 自定义图片上传
+                    images_upload_handler: ()=>{}
+                }
+            }
+        },
+        model: {
+            prop: "content",
+            event: "change"
+        },
+        props: {
+            content: {
+                type: String,
+                default: ""
+            }
+        }
+    },
+
+    
+</script>
---
+++ src/views/dynamic/Detail.vue
@@ -0,0 +1,92 @@
+<!--
+ * @Description: 高盈动态详情页
+ * @Author: Harry
+ * @Date: 2020-12-15 15:52:53
+ * @LastEditTime: 2020-12-15 17:46:41
+ * @LastEditors: Harry
+-->
+<template>
+  <!-- hidden PageHeaderWrapper title demo -->
+  <page-header-wrapper :title="false" >
+    <a-card :body-style="{padding: '24px 32px'}" :bordered="false">
+      <a-form @submit="handleSubmit" :form="form">
+        <a-form-item
+          label="选择日期"
+          :labelCol="{lg: {span: 7}, sm: {span: 7}}"
+          :wrapperCol="{lg: {span: 10}, sm: {span: 17} }">
+          <a-date-picker
+            v-decorator="[
+              'date',
+              {rules: [{ required: true, message: '请选择' }]}
+            ]"
+            name="name"
+            style="width: 100%"
+            placeholder="请选择日期" />
+        </a-form-item>
+         <a-form-item
+          label="发布状态"
+          :labelCol="{lg: {span: 7}, sm: {span: 7}}"
+          :wrapperCol="{lg: {span: 10}, sm: {span: 17} }"
+          :required="false"
+        >
+          <a-radio-group v-decorator="['target', { initialValue: 1 }]">
+            <a-radio :value="1">发布</a-radio>
+            <a-radio :value="2">下架</a-radio>
+          </a-radio-group>
+        </a-form-item>
+        <a-form-item
+          label="文章标题"
+          :labelCol="{lg: {span: 7}, sm: {span: 7}}"
+          :wrapperCol="{lg: {span: 10}, sm: {span: 17} }">
+          <a-textarea
+            rows="4"
+            v-decorator="[
+              'description',
+              {rules: [{ required: true, message: '请输入文章标题' }]}
+            ]" />
+        </a-form-item>
+        <a-form-item
+          label="文章内容"
+          :labelCol="{lg: {span: 7}, sm: {span: 7}}"
+          :wrapperCol="{lg: {span: 10}, sm: {span: 17} }">
+          <a-textarea
+            rows="4"
+            placeholder="请输入衡量标准"
+            v-decorator="[
+              'type',
+              {rules: [{ required: true, message: '请输入衡量标准' }]}
+            ]" />
+        </a-form-item>
+        <a-form-item
+          :wrapperCol="{ span: 24 }"
+          style="text-align: center"
+        >
+          <a-button htmlType="submit" type="primary">确定</a-button>
+          <a-button style="margin-left: 8px">取消</a-button>
+        </a-form-item>
+      </a-form>
+    </a-card>
+  </page-header-wrapper>
+</template>
+
+<script>
+export default {
+  name: 'BaseForm',
+  data () {
+    return {
+      form: this.$form.createForm(this)
+    }
+  },
+  methods: {
+    // handler
+    handleSubmit (e) {
+      e.preventDefault()
+      this.form.validateFields((err, values) => {
+        if (!err) {
+          console.log('Received values of form: ', values)
+        }
+      })
+    }
+  }
+}
+</script>
